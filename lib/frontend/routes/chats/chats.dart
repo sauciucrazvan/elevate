81acfac40bad6bce8ceed3ce2dd91f28d35c70fd
Razvan <razvansau9@gmail.com>
2023-09-12 10:36:16 +0300

    [PATCH] Contacts page can now be scrolled

--- lib/frontend/routes/chats/chats.dart
+++ lib/frontend/routes/chats/chats.dart
@@ -17,67 +17,69 @@ class Chats extends StatefulWidget {
 class _ChatsState extends State<Chats> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Text(
-                  "Contacts",
-                  style: Theme.of(context).textTheme.bodyLarge,
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    "Contacts",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
                 ),
-              ),
-              StreamBuilder(
-                stream: FriendsService().getFriendsStream(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    throw Exception(snapshot.error);
-                  }
+                StreamBuilder(
+                  stream: FriendsService().getFriendsStream(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      throw Exception(snapshot.error);
+                    }
 
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const CircularProgressIndicator();
-                  }
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    }
 
-                  if (!snapshot.hasData) {
-                    return const Text("You don't have any friends!");
-                  }
+                    if (!snapshot.hasData) {
+                      return const Text("You don't have any friends!");
+                    }
 
-                  final currentUid =
-                      getUsername(FirebaseAuth.instance.currentUser);
+                    final currentUid =
+                        getUsername(FirebaseAuth.instance.currentUser);
 
-                  final userList = snapshot.data!.docs
-                      .where((doc) => doc.id != currentUid)
-                      .map((doc) {
-                    return {
-                      'name': doc.id,
-                    };
-                  }).toList();
+                    final userList = snapshot.data!.docs
+                        .where((doc) => doc.id != currentUid)
+                        .map((doc) {
+                      return {
+                        'name': doc.id,
+                      };
+                    }).toList();
 
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: userList.length,
-                    itemBuilder: (context, index) {
-                      final username = userList[index]['name']!;
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: userList.length,
+                      itemBuilder: (context, index) {
+                        final username = userList[index]['name']!;
 
-                      return ChatPerson(
-                        displayName: username,
-                      );
-                    },
-                  );
-                },
-              ),
-            ],
+                        return ChatPerson(
+                          displayName: username,
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }

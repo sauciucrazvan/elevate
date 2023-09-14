537d45734eaef23bd2f3e22f2a9c950bba65bba1
Razvan <razvansau9@gmail.com>
2023-09-14 09:52:11 +0300

    [PATCH] Friends & Profile pages can now be scrolled

--- lib/frontend/routes/profile/profile.dart
+++ lib/frontend/routes/profile/profile.dart
@@ -23,98 +23,100 @@ class _ProfileState extends State<Profile> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Column(
-            children: [
-              StreamBuilder(
-                stream: AvatarService().onAvatarUpdate,
-                builder: (context, snapshot) => Avatar(
-                  username: getUsername(FirebaseAuth.instance.currentUser)!,
-                  size: 48,
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Column(
+              children: [
+                StreamBuilder(
+                  stream: AvatarService().onAvatarUpdate,
+                  builder: (context, snapshot) => Avatar(
+                    username: getUsername(FirebaseAuth.instance.currentUser)!,
+                    size: 48,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                "@${getUsername(FirebaseAuth.instance.currentUser)}",
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              const SizedBox(height: 50),
-              Text(
-                "Account Settings",
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              const SizedBox(height: 4),
-              const Divider(),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                child: Column(
-                  children: [
-                    ListTile(
-                      title: Text(
-                        "Change avatar",
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      ),
-                      trailing: Icon(
-                        Icons.account_circle,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                      onTap: () async {
-                        bool completed = await avatarService.pickAvatar();
-                        if (!completed) {
-                          showElevatedNotification(
-                            context,
-                            "There was an error uploading your avatar.",
-                            Colors.red,
-                          );
-                        } else {
-                          showElevatedNotification(
-                            context,
-                            "Avatar changed successfully.",
-                            Colors.lightGreen.shade800,
-                          );
-                        }
-                      },
-                      tileColor: Theme.of(context).colorScheme.secondary,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4)),
-                    ),
-                    const SizedBox(height: 4),
-                    ListTile(
-                      title: Text(
-                        "Sign out",
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      ),
-                      trailing: Icon(
-                        Icons.logout,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                      onTap: () => showDialog(
-                        context: context,
-                        builder: (context) => ConfirmDialog(
-                          title: "Are you sure you want to logout?",
-                          confirm: () {
-                            FirebaseAuth.instance.signOut();
-                            Navigator.pop(context);
-                          },
+                const SizedBox(height: 8),
+                Text(
+                  "@${getUsername(FirebaseAuth.instance.currentUser)}",
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                const SizedBox(height: 50),
+                Text(
+                  "Account Settings",
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                const SizedBox(height: 4),
+                const Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Text(
+                          "Change avatar",
+                          style: Theme.of(context).textTheme.bodyMedium,
                         ),
+                        trailing: Icon(
+                          Icons.account_circle,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        onTap: () async {
+                          bool completed = await avatarService.pickAvatar();
+                          if (!completed) {
+                            showElevatedNotification(
+                              context,
+                              "There was an error uploading your avatar.",
+                              Colors.red,
+                            );
+                          } else {
+                            showElevatedNotification(
+                              context,
+                              "Avatar changed successfully.",
+                              Colors.lightGreen.shade800,
+                            );
+                          }
+                        },
+                        tileColor: Theme.of(context).colorScheme.secondary,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4)),
                       ),
-                      tileColor: Theme.of(context).colorScheme.secondary,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4)),
-                    ),
-                  ],
+                      const SizedBox(height: 4),
+                      ListTile(
+                        title: Text(
+                          "Sign out",
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        trailing: Icon(
+                          Icons.logout,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        onTap: () => showDialog(
+                          context: context,
+                          builder: (context) => ConfirmDialog(
+                            title: "Are you sure you want to logout?",
+                            confirm: () {
+                              FirebaseAuth.instance.signOut();
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                        tileColor: Theme.of(context).colorScheme.secondary,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4)),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              const Divider(),
-            ],
+                const Divider(),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
